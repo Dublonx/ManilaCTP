e6ff69584318d470e312911dc56aa81d110f0e1e
Dublonx <dublonjmx17@gmail.com>
2026-08-26 18:03:46 +0800

    Update script file

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 // scroll navigation bar
 const navigation = document.querySelector('#navigation');
+const navLinks = document.querySelectorAll('.nLink');
 
 window.addEventListener("scroll", () => {
     // if
     if (window.scrollY > 300) {
         navigation.classList.add("scroll");
+        navLinks.forEach(navLink => {
+            navLink.classList.add("color");
+        });
     } else {
         navigation.classList.remove("scroll");
+        navLinks.forEach(navLink => {
+            navLink.classList.remove("color");
+        });
     }
 });
